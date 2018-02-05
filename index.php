34d28f5168605d21baf2ecb6b2c0a5b67d7cb6d1
Loovark <silverdragon999@yandex.ru>
2018-02-05 14:59:25 +0300

    module2-task2

--- index.php
+++ index.php
@@ -45,8 +45,6 @@ $lots_list = [
         'image' => 'img/lot-6.jpg'
     ]
 ];
-$lot_count = count($lots_list);
-$index = 0;
 
 ?>
 <!DOCTYPE html>
@@ -127,18 +125,24 @@ $index = 0;
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php foreach ($lots_list as $key => $val): ?>
+            <?php foreach ($lots_list as $lot): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?= $val['image']; ?>" width="350" height="260" alt="<?= $val['name']; ?>">
+                        <img src="<?= $lot['image']; ?>" width="350" height="260" alt="<?= $lot['name']; ?>">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?= $val['category']; ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $val['name']; ?></a></h3>
+                        <span class="lot__category"><?= $lot['category']; ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $lot['name']; ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?= $val['price']; ?><b class="rub">р</b></span>
+                                <span class="lot__cost">
+                                    <?php if ($lot['price'] > 1000): ?>
+                                        <?= number_format(ceil($lot['price']), 0, ',', ' ') . " " . "₽"; ?>
+                                    <?php else: ?>
+                                        <?= ceil($lot['price']) . " " . "₽"; ?>
+                                    <?php endif; ?>
+                                </span>
                             </div>
                             <div class="lot__timer timer">
                             </div>
