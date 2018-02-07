0bd125bb2fcca05c79abda761c7786e20cbb1bea
Loovark <silverdragon999@yandex.ru>
2018-02-07 17:44:36 +0300

    module3-task1

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function get_template($filename, $array) {
+
+    if ($filename[true]) {
+        require_once();
+
+        return($filename);
+    }
+    else {
+        print "";
+    }
+
+}
+
+?>
