73853b0554cfc0de07bfc0247b4afa7e08a2ff8e
Loovark <silverdragon999@yandex.ru>
2018-02-11 22:56:10 +0300

    module3-task2

--- functions.php
+++ functions.php
@@ -22,12 +22,8 @@ function set_price($price) {
 
 function time_counter () {
     date_default_timezone_set('Europe/Moscow');
-    $s_min = 60;
-    $s_hour = $s_min * 60;
-    $cur_time = time();
-    $deadline = strtotime('tomorrow');
-    $time_left = $deadline - $cur_time;
-    $hours_left = floor($time_left / $s_hour);
-    $min_left = floor($time_left % $s_min);
-    return ($hours_left.':'.$min_left);
+    $time_left = strtotime('tomorrow') - time();
+    $hours = floor($time_left / 3600);
+    $minutes = floor(($time_left / 60) - ($hours * 60));
+    return ($hours.':'.$minutes);
 }
