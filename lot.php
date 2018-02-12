3b882553c657d43cb67cc916658c0f212e8982c0
Loovark <silverdragon999@yandex.ru>
2018-02-12 22:25:47 +0300

    module4-task1

--- lot.php
+++ lot.php
@@ -1,2 +1,36 @@
 <?php
+require_once('functions.php');
 require_once ('lots_list.php');
+
+$lot = null;
+
+if (isset($_GET['lot_id'])) {
+    $lot_id = $_GET['lot_id'];
+
+    foreach ($lots_list as $item) {
+        if ($item['lot_id'] == $lot_id) {
+            $lot = $item;
+            break;
+        }
+    }
+}
+
+if (!$lot) {
+    http_response_code(404);
+}
+
+$main = render_template('templates\index.php', [
+    'lots_list' => $lots_list,
+]);
+$layout = render_template('templates\layout.php', [
+    'title' => 'Главная',
+    'is_auth' => $is_auth,
+    'user_name' => $user_name,
+    'user_avatar' => $user_avatar,
+    'main' => $main,
+    'categories' => $categories
+]);
+
+
+
+print ($layout);
