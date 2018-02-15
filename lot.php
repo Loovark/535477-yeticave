3d24e1225fb27e08208418167fbed6d9848ca459
Loovark <silverdragon999@yandex.ru>
2018-02-15 11:30:14 +0300

    module4-task1

--- lot.php
+++ lot.php
@@ -2,21 +2,15 @@
 require_once('functions.php');
 require_once ('data.php');
 
-$lot = null;
-
-$id = $lots_list[$_GET['id']] ?? http_response_code(404);
-if (isset($_GET['id'])) {
-    foreach ($lots_list as $id => $info) {
-        if ($id == $_GET['id']) {
-            $lot = $info;
-            break;
-        }
-    }
+$lot = null ?? $lots_list[$_GET['id']];
+if (!$lot) {
+    http_response_code(404);
 }
 
 $main = render_template('templates\lot.php', [
     'lot' => $lot
 ]);
+
 $layout = render_template('templates\layout.php', [
     'title' => 'Информация о лоте',
     'is_auth' => $is_auth,
